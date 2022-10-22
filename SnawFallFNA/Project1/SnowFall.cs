1f9e7b952cd96c4be61c666e8c7b640d99f512b8
SlavaNik16 <70444635+SlavaNik16@users.noreply.github.com>
2022-10-22 14:02:28 +0300

    Add files via upload

---
+++ SnawFallFNA/Project1/SnowFall.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+
+namespace Project1
+{
+    public class SnowFall : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graf;
+        SpriteBatch sprite;
+        private static KeyboardState keyboardState = Keyboard.GetState();
+        private static KeyboardState lastKeyboardState;
+
+        private static MouseState mouseState;
+        Random rnd;
+        Texture2D imageTextureBack, imageTextureSnow;
+        Vector2 posBack;
+        public List<Snow> snowflakes;
+        public SnowFall()
+        {
+            rnd = new Random();
+            graf = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            graf.PreferredBackBufferWidth = 1280;
+            graf.PreferredBackBufferHeight = 1024;
+            graf.IsFullScreen = true;
+            IsMouseVisible = true;
+            graf.ApplyChanges();
+            snowflakes = new List<Snow>();
+            AddCreateSnow();
+           
+        }
+        protected override void Initialize()
+        {
+            posBack = new Vector2(0, 0);
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            sprite = new SpriteBatch(GraphicsDevice);
+            imageTextureBack = Texture.Load("..\\Background1.png", Content);
+            imageTextureSnow = Texture.Load("..\\Snow1.png", Content);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            var lastGameTime = TimeSpan.FromMilliseconds;
+            lastKeyboardState = keyboardState;
+            keyboardState = Keyboard.GetState();
+
+            mouseState = Mouse.GetState();
+            
+            foreach (var snowflake in snowflakes)
+            {
+                snowflake.Y += snowflake.Severity;
+                if (snowflake.Y > graf.PreferredBackBufferHeight)
+                {
+                    snowflake.Y = -snowflake.Y;
+                    snowflake.X = rnd.Next(0, graf.PreferredBackBufferWidth);
+                }
+            }
+            if (KeyPressed(Keys.Escape))
+            {
+               this.Exit();
+            }
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                snowflakes.Clear();
+                AddCreateSnow();
+            }
+
+            var GameTime = gameTime.TotalGameTime.TotalMilliseconds;
+            if ((GameTime - lastGameTime) < 500)
+            {
+                base.Update(gameTime);
+                GameTime = 0;
+                lastGameTime = 0;
+            }
+           
+        }
+        public static bool KeyPressed(Keys input)
+        {
+            if (keyboardState.IsKeyDown(input) && !lastKeyboardState.IsKeyDown(input))
+            {
+                return true;
+            }
+            return false;
+
+        }
+       
+        public void AddCreateSnow()
+        {
+            var rnd = new Random();
+            
+            for (int i = 0; i < 5000; i++)
+            {
+                snowflakes.Add(new Snow
+                {
+                    X = rnd.Next(graf.PreferredBackBufferWidth),
+                    Y = -rnd.Next(graf.PreferredBackBufferHeight),
+                    Severity = rnd.Next(5, 30),
+                    
+                }) ;
+            }
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            sprite.Begin();
+            sprite.Draw(imageTextureBack, new Rectangle(0, 0, graf.PreferredBackBufferWidth, graf.PreferredBackBufferHeight), Color.White);
+            foreach (var c in snowflakes)
+            {
+                if (c.Y > 0)
+                {
+                    sprite.Draw(imageTextureSnow, new Rectangle(c.X, c.Y, c.Severity, c.Severity), Color.White);
+                }
+            }
+            sprite.End();
+            base.Draw(gameTime);
+        }
+    }
+}
