957957b5c533b9f9cb77e9dd95cd4558020a3efc
SlavaNik16 <70444635+SlavaNik16@users.noreply.github.com>
2022-10-23 23:38:02 +0300

    Delete SnawFallFNAv1.0 directory

--- SnawFallFNAv1.0/Snow.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SnawFallFNAv1
-{
-    public class Snow
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public int Severity { get; set; }
-    }
-}
